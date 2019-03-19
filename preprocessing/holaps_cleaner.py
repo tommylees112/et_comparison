e2c67e15bb736281d7b46d8cec072676a0b91181
tommylees112 <thomas.lees112@gmail.com>
2019-03-19 17:09:38 +0000

    update the preprocessing, fix bugs -- preprocessing.py --

--- preprocessing/holaps_cleaner.py
+++ preprocessing/holaps_cleaner.py
@@ -105,6 +105,8 @@ class HolapsCleaner(Cleaner):
         self.convert_units()
         # rename data
         self.rename_xr_object("holaps_evapotranspiration")
+        # resample the time units
+        self.resample_time()
         # save the netcdf file (used as reference data for MODIS and GLEAM)
         save_netcdf(
             self.clean_data, filepath=self.base_data_path / "holaps_EA_clean.nc",
--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -14,7 +14,14 @@ if __name__ == "__main__":
 
     # merge the preprocessed data and save to netcdf
     ds = merge_data_arrays(h.clean_data, g.clean_data, m.clean_data)
-    ds = get_all_valid(ds, ds.holaps_evapotranspiration, ds.modis_evapotranspiration, ds.gleam_evapotranspiration)
+    # ds = get_all_valid(ds, ds.holaps_evapotranspiration, ds.modis_evapotranspiration, ds.gleam_evapotranspiration)
+valid_mask = (
+    ds.holaps_evapotranspiration.notnull()
+    & ds.modis_evapotranspiration.notnull()
+    & ds.gleam_evapotranspiration.notnull()
+)
+
+
 
     output_ds_path='/soge-home/projects/crop_yield/EGU_compare/processed_ds.nc'
     save_netcdf(ds, output_ds_path, force=True)
